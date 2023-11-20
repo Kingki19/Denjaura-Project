5e081c2ea68bbc435d9e1d98e917f7ffa359ab70
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2023-11-21 03:41:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
 # Aplikasi Streamlit
-st.title("Aplikasi Interaktif Sederhana")
+st.title("Denjaura Project")
 
-# Tambahkan elemen-elemen interaktif
-user_input = st.text_input("Masukkan teks:")
-st.write("Teks yang dimasukkan:", user_input)
+# Input File or link 
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
